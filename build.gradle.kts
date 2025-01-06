a5d628675dc6e09716b389b754efe2fad11cbbcd
leesh5000 <leesh50000@gmail.com>
2025-01-06 19:35:14 +0900

    #7 - Lombok 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,10 @@ dependencies {
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:mysql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
